7b2212ddd536df1d3c8c7cebf3e99a2dc45089ac
mmacken4 <macilee_mackenzie@brown.edu>
2024-03-01 15:46:57 -0500

    feat: added tests

--- mock/tests/unit/basic-example.spec.ts
+++ mock/tests/unit/basic-example.spec.ts
@@ -3,6 +3,8 @@
 */
 
 import { expect, test } from "vitest";
+//import REPLInput from './REPLInput';
+import { getMockedData } from "./REPLInput.tsx";
 
 // all exports from main will now be available as main.X
 // import * as main from '../mock/src/main';
@@ -15,6 +17,18 @@ test("is 1 + 1 = 2?", () => {
 // Notice how you can test vanilla TS functions using Playwright as well!
 test("main.zero() should return 0", () => {
   expect(main.zero()).toBe(0);
+});
+
+test("getMockedData function returns the correct dataset", () => {
+  const filepath1 = 'file1';
+  const expectedData = [
+    ["col1", "col2", "col3"],
+    ["1", "2", "3"],
+    ["4", "1", "6"],
+    ["7", "8", "9"],
+    ["4", "2", "6"],
+  ];
+  expect(getMockedData(filepath1).toEqual(expectedData));
 });
 
 // For more information on how to make unit tests, visit:
