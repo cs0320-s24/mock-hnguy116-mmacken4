ab8ab09be5fedfbc8fee2ad6cb8fdb70655dd96a
havi-nguyen <113549784+havi-nguyen@users.noreply.github.com>
2024-02-29 23:45:22 -0500

    feat: user story 6

--- mock/src/components/REPLInput.tsx
+++ mock/src/components/REPLInput.tsx
@@ -3,12 +3,47 @@ import "../styles/main.css";
 import { ControlledInput } from "./ControlledInput";
 
 interface REPLInputProps {
-  // TODO: Fill this with desired props... Maybe something to keep track of the submitted commands
   history: any[]; //push every command into this history array
   setHistory: Dispatch<SetStateAction<any[]>>; //use it to maintain state of list
   verbose: boolean;
   setVerbose: Dispatch<SetStateAction<boolean>>;
 }
+
+// 1. make map: string (command) - > function
+// 2. pass function as argument to command handler (which calls the command)
+
+// var command = commandProcessing.get(commandName);
+
+// var response = command(argumentsArray);
+
+// const commandList: Map<string, REPLFunction> = new Map();
+
+export interface REPLFunction {
+  (args: Array<string>): any;
+}
+
+// function addCommand(commandName: string, commandFunc: REPLFunction) {
+//   commandList.set(commandName, commandFunc);
+// }
+
+// const loadFileCommand: REPLFunction = (Array<string>) any => {
+//   let newString = commandString.trim().toLowerCase();
+//   let file_path = commandString.slice(10).trim().toLowerCase();
+//       let data = getMockedData(file_path);
+//       if (file_path.length === 0) {
+//         return "No file path given!";
+//       }
+//       if (data.length === 1 && data[0][0] === "File not found!") {
+//         return "File not found!";
+//       } else {
+//         setLoadFile(file_path);
+//         mapFiles(file_path);
+//         return "File loaded successfully!";
+//       }
+// }
+
+// addCommand("load_file", loadFileCommand);
+
 // You can use a custom interface or explicit fields or both! An alternative to the current function header might be:
 // REPLInput(history: string[], setHistory: Dispatch<SetStateAction<string[]>>)
 export function REPLInput(props: REPLInputProps) {
@@ -19,14 +54,104 @@ export function REPLInput(props: REPLInputProps) {
   const [file_history, setFileHistory] = useState<Map<string, string[][]>>(
     new Map()
   );
-  //const [html_Table, setHTMLTable] = useState<string>("");
   const [loaded_file, setLoadFile] = useState<string>("");
+  const commandMap: Map<string, REPLFunction> = new Map();
+
+  function addCommand(commandName: string, commandFunc: REPLFunction) {
+    commandMap.set(commandName, commandFunc);
+  }
+
+  const modeCommand: REPLFunction = (args: Array<string>) => {
+    //mode command is called and mode is switched
+    if (props.verbose === false) {
+      return "Brief mode!";
+    } else if (props.verbose === true) {
+      return "Verbose mode!";
+    }
+  };
+
+  const loadCommand: REPLFunction = (args: Array<string>) => {
+    // let file_path = commandString.slice(10).trim().toLowerCase();
+    // let data = getMockedData(file_path);
+    // if (file_path.length === 0) {
+    //   return "No file path given!";
+    // }
+    // if (data.length === 1 && data[0][0] === "File not found!") {
+    //   return "File not found!";
+    // } else {
+    //   setLoadFile(file_path);
+    //   mapFiles(file_path);
+    //   return "File loaded successfully!";
+    // }
+    const filepath = args[0];
+    let data = getMockedData(filepath);
+    if (filepath.length === 0) {
+      return "No filepath given.";
+    }
+    if (data.length === 1 && data[0][0] === "File not found!") {
+      return "File not found!";
+    } else {
+      setLoadFile(filepath);
+      //return filepath; use this to check args
+      return "File loaded successfully!";
+    }
+  };
+
+  const viewCommand: REPLFunction = (args: Array<string>) => {
+    if (!(loaded_file === "")) {
+      let data = getMockedData(loaded_file);
+      return makeHTMLTable(data);
+    } else {
+      return "No file loaded!";
+    }
+  };
+
+  const searchCommand: REPLFunction = (args: Array<string>) => {
+    const columnID = args[0];
+    const value = args[1];
+    let data = getMockedData(loaded_file);
+    if (columnID === "" || value === "") {
+      return "Missing parameters, unable to search file.";
+    }
+    if (data.length === 1 && data[0][0] === "File not found!") {
+      return "File not found!";
+    }
+    if (args.length > 2) {
+      return "Too many parameters given, unable to search file.";
+    }
+    if (args.length === 2) {
+      setLoadFile(loaded_file);
+      let matchedRows = search(args);
+      if (matchedRows.length === 0) {
+        return "No value found.";
+      } else {
+        return makeHTMLTable(matchedRows);
+      }
+    }
+  };
+
+  addCommand("mode", modeCommand);
+  addCommand("load_file", loadCommand);
+  addCommand("view", viewCommand);
+  addCommand("search", searchCommand);
 
   function handleSubmit(commandString: string) {
-    if (props.verbose === false) {
-      props.setHistory([...props.history, commandOutput(commandString)]);
-    } else {
-      props.setHistory([...props.history, verboseOutput(commandString)]);
+    if (commandString.length > 0) {
+      const args = commandString.split(" ");
+      const commandName = args.shift()?.toLowerCase();
+      const commandFunc = commandMap.get(commandName!);
+      if (commandFunc) {
+        if (props.verbose === false) {
+          props.setHistory([...props.history, commandFunc(args)]);
+        } else {
+          props.setHistory([
+            ...props.history,
+            verboseOutput(commandString, commandFunc, args),
+          ]);
+        }
+      } else {
+        props.setHistory([...props.history, "Command not found!"]);
+      }
     }
     setCommandString("");
   }
@@ -39,82 +164,82 @@ export function REPLInput(props: REPLInputProps) {
     }
   }
 
-  function verboseOutput(commandString: string) {
-    if (typeof commandOutput(commandString) === "string") {
+  function verboseOutput(
+    commandString: string,
+    commandFunc: REPLFunction,
+    args: Array<string>
+  ) {
+    if (typeof commandFunc(args) === "string") {
       return (
-        "Command: " +
-        commandString +
-        "\n" +
-        "Output: " +
-        commandOutput(commandString)
+        "Command: " + commandString + "\n" + "Output: " + commandFunc(args)
       );
     } else {
       let myOutput = [];
       myOutput.push("Command: " + commandString + " ");
       myOutput.push("Output: ");
-      myOutput.push(commandOutput(commandString));
+      myOutput.push(commandFunc(args));
       return myOutput;
     }
   }
 
-  function commandOutput(commandString: string): any {
-    let newString = commandString.trim().toLowerCase();
+  // function commandOutput(commandString: string): any {
+  //   let newString = commandString.trim().toLowerCase();
 
-    // *** MODE COMMAND ***
-    if (newString.includes("mode") && newString.length === 4) {
-      if (props.verbose === false) {
-        return "Brief mode!";
-      } else if (props.verbose === true) {
-        return "Verbose mode!";
-      }
-    }
+  //   // *** MODE COMMAND ***
+  //   // if (newString.includes("mode") && newString.length === 4) {
+  //   //   if (props.verbose === false) {
+  //   //     return "Brief mode!";
+  //   //   } else if (props.verbose === true) {
+  //   //     return "Verbose mode!";
+  //   //   }
+  //   // }
 
-    // *** LOAD COMMAND ***
-    if (newString.substring(0, 10) === "load_file ") {
-      let file_path = commandString.slice(10).trim().toLowerCase();
-      let data = getMockedData(file_path);
-      if (file_path.length === 0) {
-        return "No file path given!";
-      }
-      if (data.length === 1 && data[0][0] === "File not found!") {
-        return "File not found!";
-      } else {
-        setLoadFile(file_path);
-        mapFiles(file_path);
-        return "File loaded successfully!";
-      }
+  //   // *** LOAD COMMAND ***
+  //   if (newString.substring(0, 10) === "load_file ") {
+  //     let file_path = commandString.slice(10).trim().toLowerCase();
+  //     let data = getMockedData(file_path);
+  //     if (file_path.length === 0) {
+  //       return "No file path given!";
+  //     }
+  //     if (data.length === 1 && data[0][0] === "File not found!") {
+  //       return "File not found!";
+  //     } else {
+  //       setLoadFile(file_path);
+  //       mapFiles(file_path);
+  //       return "File loaded successfully!";
+  //     }
 
-      // *** VIEW COMMAND ***
-    } else if (newString === "view") {
-      if (!(loaded_file === "")) {
-        let data = getMockedData(loaded_file);
-        return makeHTMLTable(data);
-      } else {
-        return "No file loaded!";
-      }
+  //     // *** VIEW COMMAND ***
+  //   } else if (newString === "view") {
+  //     if (!(loaded_file === "")) {
+  //       let data = getMockedData(loaded_file);
+  //       return makeHTMLTable(data);
+  //     } else {
+  //       return "No file loaded!";
+  //     }
 
-      // *** SEARCH COMMAND ***
-    } else if (newString.substring(0, 7) === "search ") {
-      if (loaded_file === "") {
-        return "No file loaded!";
-      }
-      let searchParams = commandString.substring(7).trim();
-      let searchArray = searchParams.split(" ");
-      searchArray[0] = searchArray[0].trim();
-      searchArray[1] = searchArray[1].trim();
-      if (searchArray.length === 0) {
-        return "No search parameters given!";
-      } else if (searchArray.length > 2) {
-        return "Too many search parameters given!";
-      }
-      let matchedRows = search(searchArray);
-      if (matchedRows.length === 0) {
-        return "No rows found!";
-      }
-      return makeHTMLTable(matchedRows);
-    }
-    return "Command not found!";
-  }
+  //     // *** SEARCH COMMAND ***
+  //   } else if (newString.substring(0, 7) === "search ") {
+  //     if (loaded_file === "") {
+  //       return "No file loaded!";
+  //     }
+  //     let searchParams = commandString.substring(7).trim();
+  //     let searchArray = searchParams.split(" ");
+  //     searchArray[0] = searchArray[0].trim();
+  //     searchArray[1] = searchArray[1].trim();
+  //     if (searchArray.length === 0) {
+  //       return "No search parameters given!";
+  //     } else if (searchArray.length > 2) {
+  //       return "Too many search parameters given!";
+  //     }
+  //     let matchedRows = search(searchArray);
+  //     if (matchedRows.length === 0) {
+  //       return "No rows found!";
+  //     }
+  //     return makeHTMLTable(matchedRows);
+  //   }
+  //   return "Command not found!";
+  // }
 
   function mapFiles(filePath: string) {
     const dataset = getMockedData(filePath);
@@ -140,21 +265,6 @@ export function REPLInput(props: REPLInputProps) {
     );
   }
 
-  // let searchParams = commandString.substring(7).trim();
-  // let searchArray = searchParams.split(" ");
-  // searchArray[0] = searchArray[0].trim();
-  // searchArray[1] = searchArray[1].trim();
-  // if (searchArray.length === 0) {
-  //   return "No search parameters given!";
-  // } else if (searchArray.length > 2) {
-  //   return "Too many search parameters given!";
-  // }
-  // let matchedRows = search(searchArray);
-  // if (matchedRows.length === 0) {
-  //   return "No rows found!";
-  // }
-  // return makeHTMLTable(matchedRows);
-
   function search(searchParams: string[]): string[][] {
     if (loaded_file === "file1") {
       if (searchParams.length === 2) {
@@ -173,8 +283,6 @@ export function REPLInput(props: REPLInputProps) {
             ["4", "1", "6"],
             ["4", "2", "6"],
           ];
-          // let matchedRows = [["4", "1", "6"],["4", "2", "6"]];
-          // return makeHTMLTable(matchedRows);
         }
       }
     } else if (loaded_file === "file2") {
@@ -185,8 +293,6 @@ export function REPLInput(props: REPLInputProps) {
             ["beep", "boop", "beep"],
             ["boo", "boop", "bee"],
           ];
-          // let matchedRows = [["beep", "boop", "beep"],["boo", "boop", "bee"]];
-          // return makeHTMLTable(matchedRows);
         }
       } else {
         if (searchParams[0] === "ooo") {
@@ -195,8 +301,6 @@ export function REPLInput(props: REPLInputProps) {
             ["ooo", "cee", "see"],
             ["beep", "ooo", "bloo"],
           ];
-          // let matchedRows = [["ooo", "cee", "see"],["beep", "ooo", "bloo"]];
-          // return makeHTMLTable(matchedRows);
         }
       }
     } else if (loaded_file === "file4") {
@@ -204,28 +308,22 @@ export function REPLInput(props: REPLInputProps) {
         if (searchParams[0] === "6000" && searchParams[1] === "RI") {
           //search w non-existent index
           return [["Non-existent index!"]];
-          // return "add something here"
         } else if (searchParams[0] === "6000" && searchParams[1] === "eek") {
           //search w non-existent index and val
-          return [[]];
-          // return "Value does not exist in file";
+          return [["Non-existent index and value!"]];
         }
       } else if (searchParams[0] === "eek") {
         //search w non existent val
-        return [[]];
-        // return "Value does not exist in file";
+        return [["Non-existent value!"]];
       }
     } else if (loaded_file === "mtfile") {
       //any search on empty file
-      return [[]];
-      // return "Value not found";
+      return [["File is empty!"]];
     }
     return [[]];
-    // return "Value not found";
   }
 
   function getMockedData(filePath: string): string[][] {
-    // Mock implementation
     const file1 = [
       ["col1", "col2", "col3"],
       ["1", "2", "3"],
@@ -285,10 +383,6 @@ export function REPLInput(props: REPLInputProps) {
 
   return (
     <div className="repl-input">
-      {/* This is a comment within the JSX. Notice that it's a TypeScript comment wrapped in
-            braces, so that React knows it should be interpreted as TypeScript */}
-      {/* I opted to use this HTML tag; you don't need to. It structures multiple input fields
-            into a single unit, which makes it easier for screenreaders to navigate. */}
       <fieldset>
         <legend>Enter a command:</legend>
         <ControlledInput
@@ -297,8 +391,6 @@ export function REPLInput(props: REPLInputProps) {
           ariaLabel={"Command input"}
         />
       </fieldset>
-      {/* TODO WITH TA: Build a handleSubmit function that increments count and displays the text in the button */}
-      {/* TODO: Currently this button just counts up, can we make it push the contents of the input box to the history?*/}
       <button aria-label={"Submit"} onClick={() => handleSubmit(commandString)}>
         Submit
       </button>
